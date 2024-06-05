10495d6996587d4e5cd942093f51d7236573c785
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2024-06-05 20:20:12 +0200

    added user-details endpoint

--- services/usersServices.js
+++ services/usersServices.js
@@ -1,6 +1,7 @@
-import UserModel from '../db/users.model.js';
 import bcrypt from 'bcrypt';
 
+import UserModel from '../db/users.model.js';
+import Recipe from '../db/recipe.model.js';
 import { generateAvatar, processAvatar } from '../helpers/processImage.js';
 
 export const createUser = async ({ name, email, password }) => {
@@ -13,6 +14,30 @@ export const createUser = async ({ name, email, password }) => {
     password: hashPassword,
     avatarURL,
   });
+};
+
+export const getUserById = async (id, self = true) => {
+  const user = await UserModel.findById(id).select(
+    'name email avatarURL favorites followers following'
+  );
+
+  const result = {
+    name: user.name,
+    email: user.email,
+    avatarURL: user.avatarURL,
+    followersCount: user.followers.length,
+    followingCount: user.following.length,
+    recipesCount: await Recipe.countDocuments({ owner: id }),
+  };
+
+  if (!self) {
+    return result;
+  }
+
+  return {
+    ...result,
+    favorites: user.favorites.length,
+  };
 };
 
 export const updateUserById = (id, updatePayload) => {
