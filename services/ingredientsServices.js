88b66be6c9ccae0bf021e96af082af5bae70e950
Maryna-Korbet <m22v86k@gmail.com>
2024-06-09 11:18:21 -0400

    add getRecipesByIngredients and getIngredientsByName
    Added recipe search by ingredient and ingredient search by name.

--- services/ingredientsServices.js
+++ services/ingredientsServices.js
@@ -1,4 +1,29 @@
+import mongoose from 'mongoose';
 import Ingredient from '../db/ingredient.modal.js';
+import Recipe from '../db/recipe.model.js';
+
+const { ObjectId } = mongoose.Types;
+
+export const searchRecipesByIngredient = async (ingredientName) => {
+
+    const searchIngredient = await Ingredient.find({
+        name: { $regex: ingredientName, $options: "i" },
+    });
+
+    if (!searchIngredient.length) {
+        return []; 
+    }
+
+    const idIngredients = searchIngredient.map(({ _id }) => _id);
+
+    const objectIdIngredients = idIngredients.map(id => new ObjectId(id));
+
+    const result = await Recipe.find({
+        'ingredients.id': { $in: objectIdIngredients },
+    });
+
+    return result;
+};
 
 export const listIngredients = async (searchOptions = {}) => {
     const { filter = {}, fields = '', settings = {} } = searchOptions;
@@ -6,6 +31,16 @@ export const listIngredients = async (searchOptions = {}) => {
     return result;
 };
 
+export const searchIngredientsByName = async (name) => {
+    const result = await Ingredient.find({
+        name: { $regex: name, $options: "i" },
+    });
+    
+    return result;
+};
+
 export const ingredientsServices = {
+    searchRecipesByIngredient,
     listIngredients,
+    searchIngredientsByName,
 };
