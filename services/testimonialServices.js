63dfbb18012aa665a21b25e8b989993beefe3715
marychka2335 <marychka2335@icloud.com>
2024-06-09 09:42:50 +0300

    testimonials code simplified

--- services/testimonialServices.js
+++ services/testimonialServices.js
@@ -1,8 +1,3 @@
 import Testimonial from "../db/testimonial.model.js";
 
-export const getAll = (search = {}) => {
-    const { filter = {}, fields = "", settings = {} } = search;
-    return Testimonial.find(filter, fields, settings);
-
-};
 export const findTestimonials = () => Testimonial.find();
