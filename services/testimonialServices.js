771daa50a40e3b53b0c5d96e7a69925ae7adbde5
marychka2335 <marychka2335@icloud.com>
2024-06-16 20:23:09 +0300

    testimonials commit

--- services/testimonialServices.js
+++ services/testimonialServices.js
@@ -1,3 +1,21 @@
 import Testimonial from "../db/testimonial.model.js";
 
+
+
+
 export const findTestimonials = () => Testimonial.find();
+
+
+// const testimonials = await Testimonial.aggregate([
+//     {
+//         $lookup: {
+//             from: 'users',
+//             localField: 'owner',
+//             foreignField: '_id',
+//             as: 'owner',
+//         }
+
+//     },
+//     return testimonials;
+// ]);
+
