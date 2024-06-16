6aa139a0bbf3812fcc795eca7f95bb23bd42f2f2
Maryna-Korbet <m22v86k@gmail.com>
2024-06-16 08:49:53 -0400

    delete public

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -67,7 +67,7 @@ const addRecipe = async (req, res) => {
 
   const newPath = path.join(recipeImagesPath, filename);
   await fs.rename(oldPath, newPath);
-  const thumb = path.posix.join('public', 'recipeImages', filename);
+  const thumb = path.posix.join('recipeImages', filename);
 
   const result = await recipesServices.createRecipe({
     ...recipeData,
